9dc776f4de0d55d3640899051bdb3965af72b787
francis <f.slinky07@gmail.com>
2022-05-17 05:56:30 -0400

    finished main logic and design

--- app/src/main/java/com/slinky/myprofile/MainActivity.java
+++ app/src/main/java/com/slinky/myprofile/MainActivity.java
@@ -1,8 +1,20 @@
 package com.slinky.myprofile;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       ImageButton btn = findViewById(R.id.settingsWheel);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSettings();
+            }
+        });
+
     }
+    protected void launchSettings() {
+        Intent intent = new Intent(this, SpinnerActivity.class);
+        startActivity(intent);
+    }
+//    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+//            new ActivityResultContracts.StartActivityForResult(),
+//            new ActivityResultCallback<ActivityResult>() {
+//                @Override
+//                public void onActivityResult(ActivityResult result) {
+//                    if (result.getResultCode() == 0) {
+//                        Intent intent = result.getData();
+//                        ///something about ketting item from key for seasons maybe
+//                    }
+//                }
+//            }
+//    );
+
 }
--- app/src/main/java/com/slinky/myprofile/SpinnerActivity.java
+++ app/src/main/java/com/slinky/myprofile/SpinnerActivity.java
@@ -2,13 +2,59 @@ package com.slinky.myprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class SpinnerActivity extends AppCompatActivity {
-
+    private  String selected = "spring";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+        Button btn = findViewById(R.id.confirmBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // set AND cache theme then launch
+
+                launchMain();
+            }
+        });
+
+        Spinner spin = findViewById(R.id.spinnerID);
+
+        // init spinner
+        ArrayAdapter<CharSequence> stackAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.seasonsList,
+                android.R.layout.simple_spinner_item
+        );
+        stackAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(stackAdapter);
+
+        // set select listenner
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selected = spin.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // booooooo
+            }
+        });
+    }
+
+    protected void launchMain() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
